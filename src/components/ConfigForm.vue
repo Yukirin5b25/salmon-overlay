12ba9ed08f28456680056c30c73aa15fc5020e38
Lu Hui <luhui19970305@hotmail.com>
2022-10-04 03:34:31 +0800

    page headers with config modal (#4)

---
+++ src/components/ConfigForm.vue
@@ -0,0 +1,27 @@
+<template>
+  <n-form v-model="configs">
+    <n-grid x-gap="16" cols="1" >
+      <n-form-item-grid-item :span="1">
+        <n-checkbox v-model:checked="configs.blurPlayerNames">Blur Names</n-checkbox>
+      </n-form-item-grid-item>
+      <n-form-item-grid-item :span="1" label="Combatants">
+        <n-input-number v-model:value="configs.maxCombatants"></n-input-number>
+      </n-form-item-grid-item>
+    </n-grid>
+  </n-form>
+</template>
+
+<script >
+  import { NGrid, NForm, NFormItemGridItem, NInput, NInputNumber, NCheckbox } from 'naive-ui'
+  import { computed } from 'vue'
+  import { useOverlayConfigsStore } from '@/stores/overlayConfigs'
+  export default {
+    setup() {
+      const overlayConfigsStore = useOverlayConfigsStore()
+      return {
+        configs: computed(() => overlayConfigsStore.configs )
+      }
+    },
+    components: { NGrid, NForm, NFormItemGridItem, NInputNumber, NCheckbox },
+  }
+</script>
--- src/components/EncounterBar.vue
+++ src/components/EncounterBar.vue
@@ -1,21 +1,49 @@
 <template>
-  <n-divider title-placement="left" style="margin: 0 0 0 0">
-    <template #default>
-      <!-- <n-p style="margin: 0 0 0 0">{{ encounterInfo.dps }}</n-p>
-      <n-divider vertical /> -->
-      <n-p style="margin: 0 0 0 0">{{ encounterInfo.zoneName }}</n-p>
-      <n-divider vertical />
-      <n-p style="margin: 0 0 0 0">{{ encounterInfo.duration }}</n-p>
+  <n-page-header>
+    <template #title>
+      <n-space style="margin-left: -6px">
+        <n-divider vertical style="height: 1.2em"></n-divider>
+        <n-h6 style="margin: 2px 0 0 0px;">{{ encounterInfo.zoneName || 'No Data' }}</n-h6>
+        <n-divider vertical style="height: 1.2em"></n-divider>
+      </n-space>
     </template>
-  </n-divider>
+    <template #subtitle >
+      <n-space>
+        <div style="margin-top: 4px">
+          {{ encounterInfo.duration || '00:00' }}
+        </div>
+        <n-divider vertical style="height: 1.2em"></n-divider>
+      </n-space>
+    </template>
+    <template #extra>
+      <n-button ghost :bordered="false" @click="showConfigModal = true">
+        <template #icon>
+          <n-icon>
+            <IosSettings/>
+          </n-icon>
+        </template>
+      </n-button>
+    </template>
+  </n-page-header>
+  <n-modal v-model:show="showConfigModal">
+    <n-card title="Overlay Configs">
+      <ConfigForm/>
+    </n-card>
+  </n-modal>
 </template>
 
-<script lang="ts" >
-  import { NDivider, NP } from 'naive-ui'
+<script >
+  import { NDivider, NH6, NPageHeader, NSpace, NButton, NIcon, NModal, NCard } from 'naive-ui'
+  import { IosSettings } from '@vicons/ionicons4'
+  import { ref } from 'vue'
+  import ConfigForm from '@/components/ConfigForm.vue'
   export default {
-   setup() {
-   },
-   components: { NDivider, NP },
-   props: ['encounterInfo']
+    setup() {
+      return {
+        showConfigModal: ref(false)
+      }
+    },
+    components: { NDivider, NH6, NPageHeader, NSpace, NIcon, NButton, NModal, NCard, IosSettings, ConfigForm },
+    props: ['encounterInfo']
   }
 </script>
